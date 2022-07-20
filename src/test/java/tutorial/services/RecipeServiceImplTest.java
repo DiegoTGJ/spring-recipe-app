6df3dc7db6632d3d6f3cbfe52b42482df07c6027
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-19 23:04:02 -0400

    Unit Testing with JUnit, Mockito and MockMVC.

---
+++ src/test/java/tutorial/services/RecipeServiceImplTest.java
@@ -0,0 +1,37 @@
+package tutorial.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tutorial.domain.Recipe;
+import tutorial.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(),1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
